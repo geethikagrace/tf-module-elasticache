740cb4dea125860885a874177abbd262bc66e39f
geethika <geethilka2219@gmail.com>
2023-06-26 23:12:37 +0530

    RDS

--- main.tf
+++ main.tf
@@ -38,18 +38,7 @@ resource "aws_elasticache_parameter_group" "main" {
   tags         = merge(var.tags, { Name = "${var.name}-${var.env}-pg" })
 }
 
-resource "aws_elasticache_cluster" "main" {
-  cluster_id           = "${var.name}-${var.env}-elasticache"
-  engine               = "redis"
-  node_type            = var.node_type
-  num_cache_nodes      = var.num_cache_nodes
-  parameter_group_name = aws_elasticache_parameter_group.main.name
-  engine_version       = var.engine_version
-  port                 = 6379
-  subnet_group_name    = aws_elasticache_subnet_group.main.name
-  security_group_ids   = aws_security_group.main.id
-  tags         = merge(var.tags, { Name = "${var.name}-${var.env}-elasticache" })
-}
+
 
 resource "aws_elasticache_replication_group" "main" {
   replication_group_id       = "${var.name}-${var.env}-elasticache"
