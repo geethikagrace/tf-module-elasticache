a6576d84cc341b7846101563e74d5f98233fa0ae
geethika <geethilka2219@gmail.com>
2023-07-11 19:40:09 +0530

    monitor cidr

--- main.tf
+++ main.tf
@@ -31,12 +31,12 @@ resource "aws_security_group" "main" {
   tags =  merge(var.tags, { Name = "${var.name}-${var.env}-sg" })
 }
 
-resource "aws_elasticache_parameter_group" "main" {
-  family       = "redis6.x"
-  name         = "${var.name}-${var.env}-pg"
-  description  = "${var.name}-${var.env}-pg"
-  tags         = merge(var.tags, { Name = "${var.name}-${var.env}-pg" })
-}
+#resource "aws_elasticache_parameter_group" "main" {
+#  family       = "redis6.x"
+#  name         = "${var.name}-${var.env}-pg"
+#  description  = "${var.name}-${var.env}-pg"
+#  tags         = merge(var.tags, { Name = "${var.name}-${var.env}-pg" })
+#}
 
 resource "aws_elasticache_parameter_group" "main" {
   family = "redis6.x"
